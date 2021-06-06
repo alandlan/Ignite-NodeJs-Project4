edccdc6f108e3fec43524a8cfcdf4a7f8fc3eccd
Alan Martins <alanhorcrux@gmail.com>
2021-06-06 13:55:09 -0300

    Criando rota para salvar carro

--- src/shared/infra/typeorm/migrations/1622977528216-CreateCars.ts
+++ src/shared/infra/typeorm/migrations/1622977528216-CreateCars.ts
@@ -20,7 +20,7 @@ export class CreateCars1622977528216 implements MigrationInterface {
             type: "varchar",
           },
           {
-            name: "daily,rate",
+            name: "daily_rate",
             type: "numeric",
           },
           {
