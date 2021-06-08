b5f248a50efa3e21788d750994e13a63cf34ed18
Alan Martins <alanhorcrux@gmail.com>
2021-06-08 15:28:02 -0300

    TDD - Listando carros disponíveis por marca

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -12,7 +12,7 @@ describe("List Cars", () => {
   });
 
   it("shuold be able to list all available cars", async () => {
-    const car = carsRepository.create({
+    const car = await carsRepository.create({
       brand: "Car Brand",
       category_id: "category_id",
       daily_rate: 100.0,
@@ -22,7 +22,25 @@ describe("List Cars", () => {
       name: "Car1",
     });
 
-    const cars = await listCarsUseCase.execute();
+    const cars = await listCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by name", async () => {
+    const car = await carsRepository.create({
+      brand: "Car_brand",
+      category_id: "category_id",
+      daily_rate: 100.0,
+      description: "Car Description",
+      fine_amount: 60,
+      license_plate: "DEF-1213",
+      name: "Car1",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      brand: "Car_brand",
+    });
 
     expect(cars).toEqual([car]);
   });
