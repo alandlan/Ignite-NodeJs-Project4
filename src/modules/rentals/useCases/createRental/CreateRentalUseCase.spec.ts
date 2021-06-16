194dc2ae62209c0218960934de0a5716659f7c30
Alan Martins <alanhorcrux@gmail.com>
2021-06-16 18:16:27 -0300

    TDD - Create a rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import dayjs from "dayjs";
 
 import { AppError } from "@shared/errors/AppError";
 
@@ -8,22 +9,23 @@ let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 
 describe("Create Rental", () => {
+  const dayAdd24Hours = dayjs().add(1, "day").toDate();
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
     createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
 
     createRentalUseCase.execute({
-      user_id: "1234",
-      car_id: "12345",
-      expected_return_date: new Date(),
+      user_id: "1",
+      car_id: "1",
+      expected_return_date: dayAdd24Hours,
     });
   });
 
   it("should be able to create a new rental", async () => {
     const rental = await createRentalUseCase.execute({
-      user_id: "12345",
-      car_id: "123456",
-      expected_return_date: new Date(),
+      user_id: "2",
+      car_id: "2",
+      expected_return_date: dayAdd24Hours,
     });
 
     expect(rental).toHaveProperty("id");
@@ -32,9 +34,9 @@ describe("Create Rental", () => {
   it("should not be able to create a new rental if there is another open to the same user", async () => {
     expect(async () => {
       const rental = await createRentalUseCase.execute({
-        user_id: "1234",
-        car_id: "123456",
-        expected_return_date: new Date(),
+        user_id: "1",
+        car_id: "3",
+        expected_return_date: dayAdd24Hours,
       });
     }).rejects.toBeInstanceOf(AppError);
   });
@@ -42,8 +44,18 @@ describe("Create Rental", () => {
   it("should not be able to create a new rental if there is another open to the same car", async () => {
     expect(async () => {
       const rental = await createRentalUseCase.execute({
-        user_id: "1234567",
-        car_id: "12345",
+        user_id: "3",
+        car_id: "1",
+        expected_return_date: dayAdd24Hours,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a new rental with invalid return time", async () => {
+    expect(async () => {
+      const rental = await createRentalUseCase.execute({
+        user_id: "4",
+        car_id: "4",
         expected_return_date: new Date(),
       });
     }).rejects.toBeInstanceOf(AppError);
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,7 +1,11 @@
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 
 import { AppError } from "@shared/errors/AppError";
+
+dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -31,6 +35,19 @@ class CreateRentalUseCase {
 
     if (rentalIpenToUser) {
       throw new AppError("There's a rental in progress for user!");
+    }
+
+    const expectedReturnDateFormat = dayjs(expected_return_date)
+      .utc()
+      .local()
+      .format();
+
+    const dateNow = dayjs().utc().local().format();
+
+    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+
+    if (compare < 24) {
+      throw new AppError("Invalid return time!");
     }
 
     const rental = await this.rentalsRepository.create({
