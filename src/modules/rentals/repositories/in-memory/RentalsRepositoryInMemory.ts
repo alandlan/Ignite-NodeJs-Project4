63d7242449b1ef4fc2f9240380ff13d4988bd79c
Alan Martins <alanhorcrux@gmail.com>
2021-06-16 17:47:59 -0300

    TDD - Create a rental

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -25,14 +25,14 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     const rental = await this.rentals.find(
-      (rental) => rental.car_id === car_id && rental.end_date === null
+      (rental) => rental.car_id === car_id && !rental.end_date
     );
 
     return rental!;
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
     const rental = await this.rentals.find(
-      (rental) => rental.user_id === user_id && rental.end_date === null
+      (rental) => rental.user_id === user_id && !rental.end_date
     );
 
     return rental!;
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
@@ -9,6 +11,12 @@ describe("Create Rental", () => {
   beforeEach(() => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
     createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+
+    createRentalUseCase.execute({
+      user_id: "1234",
+      car_id: "12345",
+      expected_return_date: new Date(),
+    });
   });
 
   it("should be able to create a new rental", async () => {
@@ -19,5 +27,25 @@ describe("Create Rental", () => {
     });
 
     expect(rental).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new rental if there is another open to the same user", async () => {
+    expect(async () => {
+      const rental = await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "123456",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a new rental if there is another open to the same car", async () => {
+    expect(async () => {
+      const rental = await createRentalUseCase.execute({
+        user_id: "1234567",
+        car_id: "12345",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
